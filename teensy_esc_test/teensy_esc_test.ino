8f3dc4247be6c52c562a4616f0b08d86cfbfda89
Eugene Lee <elee.jindo@gmail.com>
2026-04-25 10:47:50 -0600

    Labeled files by board.
    Available libraries differ between Arduino Uno and Teensy 4.1 based on hardware. I think it's best to identify which of my files were written for which board.

--- teensy_esc_test/teensy_esc_test.ino
+++ teensy_esc_test/teensy_esc_test.ino
@@ -15,7 +15,6 @@ APISQUEEN 12-24V 45A Bi-directional ESC
 1.5-2ms Forward
 1-1.5ms Backward
 1.5ms Zero Throttle
-Calibrate ESC by sending 1.5ms signal for zero throttle at power on, then forward throttle, then backward.
 */
 
 void setup() {
@@ -23,9 +22,47 @@ void setup() {
   analogWriteFrequency(signalPin, 500);
   // set pulse width resolution to 8 bits
   analogWriteResolution(8);
-
   // begin serial communication at 9600 bits/sec
   Serial.begin(9600);
+  // 3 second delay written into calibration for stability
+  calibrate_esc();
+} // end setup
+
+/*
+Relationship between value sent to signal pin and resultduty cycle is described below:
+
+W Pulse Width
+C Control Signal
+R Resolution
+F Frequency
+W = C / R*F
+W = C / 256*500
+*/
+
+void loop() {
+  // read joystick values from pins
+  // int xValue = analogRead(xPin);
+  int yValue = analogRead(yPin);
+  // int buttonValue = digitalRead(buttonPin);
+
+  // map the y-axis potentiometer ranging from 0-1023 to a valid pulse width range from 128-255
+  int pwmVal = map(yValue, 0, 1023, 128, 255);
+  // write pulse width to signal pin
+  analogWrite(signalPin, pwmVal);
+
+  //Serial.print(xValue);
+  //Serial.print("\t");
+  Serial.print(yValue);
+  //Serial.print("\t");
+  //Serial.print(buttonValue);
+  Serial.println("");
+
+  // 10 ms delay for stability
+  delay(10);
+} // end loop
+
+// calibrate ESC by sending 1.5ms signal for zero throttle at power on, then forward throttle, then backward.
+void calibrate_esc() {
   Serial.println("Calibrating ESCs...");
 
   // begin at zero throttle for three seconds
@@ -48,35 +85,4 @@ void setup() {
 
   Serial.println("Calibration complete.");
   delay(3000);
-} // end setup
-
-/*
-W Pulse Width
-C Control Signal
-R Resolution
-F Frequency
-W = C / R*F
-W = C / 256*500
-*/
-
-void loop() {
-  //read joystick values from pins
-  // int xValue = analogRead(xPin);
-  int yValue = analogRead(yPin);
-  // int buttonValue = digitalRead(buttonPin);
-
-  // map the y-axis potentiometer ranging from 0-1023 to a valid pulse width range from 128-255
-  int pwmVal = map(yValue, 0, 1023, 128, 255);
-  // write pulse width to signal pin
-  analogWrite(signalPin, pwmVal);
-
-  //Serial.print(xValue);
-  //Serial.print("\t");
-  Serial.print(yValue);
-  //Serial.print("\t");
-  //Serial.print(buttonValue);
-  Serial.println("");
-
-  // 10 ms delay for stability
-  delay(10);
-} // end loop
+}
